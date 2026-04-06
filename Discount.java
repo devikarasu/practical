e953083362f6815412b916a4e7eefb069bec12a7
devikarasu <147230683+devikarasu@users.noreply.github.com>
2026-04-06 15:36:22 +0530

    Add files via upload

---
+++ Discount.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class Discount{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int amount = sc.nextInt();
+
+        if (amount >= 5000)
+            System.out.println("20% Discount");
+        else if (amount >= 2000)
+            System.out.println("10% Discount");
+        else
+            System.out.println("No Discount");
+    }
+}
